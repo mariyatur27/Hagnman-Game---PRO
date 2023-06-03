b50419abc9666b089c483430917ba3f25e509356
mariyatur27 <63456223+mariyatur27@users.noreply.github.com>
2023-06-02 21:55:12 -0400

    verifying input

--- script.js
+++ script.js
@@ -1,34 +1,55 @@
 const word_bank = [
-    "country",
-    "world",
+    "alabama",
+    "america",
     "school",
     "job",
-    "computer"
+    "racoon"
 ]
 
 let word_input_obj;
 let score = document.getElementById('tries-left');
+let used_letters_arr=[];
 
 const tries_left = () => {
 
 }
 
-const checkInput = (id) => {
+const checkInput = (id, word_arr) => {
     var input = document.getElementById(id).value;
     var correct_letter = word_input_obj[id];
-    var used_letters_arr=[];
     var score = Object.keys(word_input_obj).length;
 
+    //check if the letter is wrong
     if (input != correct_letter){
+        document.getElementById(id).value = '';
+        document.getElementById(id).style.background='#ff6b6b'
         if (!used_letters_arr.includes(input)){
-            document.getElementById(id).value = '';
             let input_ele = document.createElement('h4'); input_ele.innerHTML = input;
             document.getElementById('incorrect-letters').appendChild(input_ele);
             used_letters_arr.push(input);
         }
     }
+    //if the letter is right
+    else{
+        //counting the number of times this letter occurs in a word
+        var occurences = word_arr.filter(x => x == input).length;
+        //looping through the object
+        if (occurences != 0){
+            for(const key in word_input_obj){
+                if (word_input_obj[key] == input){
+                    document.getElementById(key).value = input;
+                    document.getElementById(key).style.background='#91ff91'
+                    document.getElementById(key).readOnly = true;
+                }
+            }
+        }
 
-    console.log(used_letters_arr)
+        document.getElementById(id).readOnly = true;
+        document.getElementById(id).style.background='#91ff91'
+        if (!used_letters_arr.includes(input)){
+            used_letters_arr.push(input);
+        }
+    }
 }
 
 const createDataset = (word_arr) => {
@@ -48,7 +69,7 @@ const buildScreen = (word) => {
             letter_box.classList.add('letter-box'); letter_box.setAttribute('maxlength', '1'); letter_box.id='letter'.concat(i);
 
             letter_box.addEventListener('input', function() {
-                checkInput(letter_box.id)
+                checkInput(letter_box.id, word.split(''))
             })
 
             container.appendChild(letter_box);
